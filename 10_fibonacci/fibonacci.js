a1a41e145e98f635c226937e786a67b424205896
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2022-12-26 12:25:16 -0500

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	if(n < 0) { return "OOPS";}
+	if(n == 0) { return 0;}
+	
+	var a = 1;
+	var b = 1;
+	
+	for(var i = 2; i < n; i++) {
+		var temp = a + b;
+		a = b;
+		b = temp;
+	}
+	return b;
 };
 
 // Do not edit below this line
