2951f89133856468d0ead784167bc3e64a9f0085
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2022-12-26 12:50:11 -0500

    complete exercise 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+	const res = [];
+	
+	for(book of books) {
+		res.push(book.title);
+	}
+	return res;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	
+	return people.reduce((oldest, person) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+		return personAge > oldestAge? person: oldest;
+	});
 };
+
+function getAge(dob, dod) {
+	if(!dod) {
+		dod = new Date().getFullYear();
+	}
+	return dod - dob;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
