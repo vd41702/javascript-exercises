1bea3f68356a7054a11ee89d1c305850563578a9
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2022-12-26 12:02:46 -0500

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(nums) {
+	var sum = 0;
+	for(num of nums) {
+		sum += num;
+	}
+	return sum;
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(nums) {
+	var product = 1;
+	for(num of nums) {
+		product *= num;
+	}
+	return product;
 };
 
-const sum = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	result = 1;
+	for(var i = a; i > 1; i--) {
+		result *= i;
+	}
+	return result;
 };
 
 // Do not edit below this line
