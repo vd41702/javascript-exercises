5807971deed53eec255985d7c64b23c0c011f2e5
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2022-12-26 12:18:43 -0500

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+	var left = 0;
+	var right = phrase.length - 1;
+	
+	var s = phrase.split("");
 
+	while(left < right) {
+		if(s[left].toLowerCase() == s[left].toUpperCase()) {
+			left++;
+			continue;
+		}
+		if(s[right].toLowerCase() == s[right].toUpperCase()) {
+			right--;
+			continue;
+		}
+		if(s[left].toLowerCase() != s[right].toLowerCase()) {
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
 };
 
 // Do not edit below this line
